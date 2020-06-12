7a264e4608ef6a5cd47895687ecdb3885833d155
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-12 13:28:17 +0100

    Initial Commit

---
+++ FavoriteBook App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteBook App
+//
+//  Created by Emil Vaklinov on 12/06/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
